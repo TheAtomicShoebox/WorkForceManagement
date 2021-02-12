cc24b5f400d522ee6fdf15d080b126c8b158158f
KennieJ <kennie.jfive@gmail.com>
2021-02-11 20:50:25 -0500

    added ICollection

--- WorkForceManagement.Data/EmployeeRole.cs
+++ WorkForceManagement.Data/EmployeeRole.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +25,11 @@ namespace WorkForceManagement.Data
         [Required]
         public bool IsSupervisor { get; set; }
 
+        //[ForeignKey("Employee")]
+        //public int EmployeeId { get; set; }
+        //public virtual Employee Employee { get; set; }
 
+        public ICollection<Employee> Employees { get; set; }
     }
 
 }
