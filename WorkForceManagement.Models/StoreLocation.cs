43d522795ef469efc5cec24b1864bed08aac97b3
Amy Martinez <ajmartinez0712@gmail.com>
2021-02-11 19:30:23 -0500

    Store location object

---
+++ WorkForceManagement.Models/StoreLocation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkForceManagement.Models
+{
+    public class StoreLocation
+    {
+        [Key]
+        public int StoreNumber { get; set; }
+        [Required]
+        public string StoreName { get; set; }
+        [Required]
+        public string StreetAddress { get; set; }
+
+    }
+}
