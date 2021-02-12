736967875e1473409eb311a355a7d9dc58792b94
KennieJ <kennie.jfive@gmail.com>
2021-02-11 19:32:51 -0500

    EmployeeRole class add

--- WorkForceManagement.Data/EmployeeRole.cs
+++ WorkForceManagement.Data/EmployeeRole.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WorkForceManagement.Data
 {
-    class EmployeeRole
+    public class EmployeeRole
     {
+        [Key]
+        public int RoleId { get; set; }
+
+        [Required]
+        public string RoleName { get; set; }
+
+        [Required]
+        public string RoleDescription { get; set; }
+
+        [Required]
+        public double BaseRate { get; set; }
+
+        [Required]
+        public bool IsSupervisor { get; set; }
+
+
     }
+
 }
