c450216f9ecb6a0625e8e3c6d001d0064c524c65
TheAtomicShoebox <33351047+TheAtomicShoebox@users.noreply.github.com>
2021-02-11 21:15:36 -0500

    Merge and Fix

--- WorkForceManagement.WebAPI/App_Start/IdentityConfig.cs
+++ WorkForceManagement.WebAPI/App_Start/IdentityConfig.cs
@@ -1,9 +1,8 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
+﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
-using WorkForceManagement.WebAPI.Models;
+using WorkForceManagement.Data;
 
 namespace WorkForceManagement.WebAPI
 {
