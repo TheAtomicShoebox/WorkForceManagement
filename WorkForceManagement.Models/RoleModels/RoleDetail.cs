96a1b1db1a6af628d2e9a2aa3a9e8cad5c9e26f9
KennieJ <kennie.jfive@gmail.com>
2021-02-18 19:21:56 -0500

    2/18 commit

---
+++ WorkForceManagement.Models/RoleModels/RoleDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkForceManagement.Models.RoleModels
+{
+    public class RoleDetail
+    {
+        public int RoleId { get; set; }
+
+        public string RoleName { get; set; }
+
+        public string RoleDescription { get; set; }
+
+        public double BaseRate { get; set; }
+
+        public bool IsSupervisor { get; set; }
+    }
+}
